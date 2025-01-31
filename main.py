e68c53d86cf2849f50acccc92bd05c93f473a61f
addelian <addelia.nicolas@gmail.com>
2025-01-30 22:18:06 -0600

    char counter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     book_path = "./books/frankenstein.txt"
     file_contents = get_book_text(book_path)
-    print(count_words(file_contents))
+    word_count = count_words(file_contents)
+    char_count = count_characters(file_contents)
+    print(char_count)
 
 
 def count_words(text):
@@ -11,6 +13,17 @@ def count_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def count_characters(text):
+    lowered = text.lower()
+    counter = {}
+    for c in lowered:
+        if c in counter:
+            counter[c] += 1
+        else:
+            counter[c] = 1
+    return counter
 
 
 main()
